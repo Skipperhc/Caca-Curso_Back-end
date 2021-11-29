83fa78c842fd6cdbb075aadca2aecfb9e491ec37
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2021-11-29 00:19:05 -0300

    back configurado para o maestro

--- routes/AvaliacaoRoutes.js
+++ routes/AvaliacaoRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', avalicaoController.getAll);
 router.get('/id/:id', avalicaoController.getById);
-router.get('/cursousuario', AvalicaoGeralController.getByIdCursoUsuario);
+router.get('/cursousuario', avalicaoController.getByIdCursoUsuario);
 router.post('/', avalicaoController.create);
 router.put('/', avalicaoController.update);
 router.delete('/id/:id', avalicaoController.remove);
